a7fb4aa951d5346d24739ad3d0b0daf8b966aab1
unknown <jeivi.777@gmail.com>
2023-09-28 09:34:54 -0300

    feat: Add Core Components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://avatars.githubusercontent.com/u/133774420?v=4',
+    alt: 'João Sousa'  
+},
+argTypes: {
+    src: {
+        control: {
+            type: 'text'
+        }
+    }
+}
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+    args: {
+        src: undefined
+    }
+}
+
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+    title: 'Typography/Text',
+    component: Text,
+    args: {
+        size: 'md',
+        children: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti praesentium eius vitae, iusto modi autem atque aperiam sint eos optio corporis sapiente, temporibus quis impedit maxime itaque eum esse explicabo?'
+    },
+    argTypes: {
+        size: {
+            options: ['xxs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '4xl', '5xl', '6xl', '7xl', '8xl', '9xl'],
+            control: { type: 'inline-radio' }
+          },
+    }
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+
+    args: {
+        children: 'Strong text',
+        as: 'strong'
+    }
+}
