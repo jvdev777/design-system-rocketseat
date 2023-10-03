716b3a27d8c08e19c4a72099aa383d795c692fcf
unknown <jeivi.777@gmail.com>
2023-10-03 08:59:44 -0300

    .

--- packages/docs/storybook-static/assets/Avatar.stories-e1a0b0c4.js
+++
@@ -1,6 +0,0 @@
-import{A as i}from"./index-1b04699c.js";import"./index-c0290abd.js";import"./_commonjsHelpers-725317a4.js";import"./extends-98964cd2.js";const l={title:"Data display/Avatar",component:i,args:{src:"https://avatars.githubusercontent.com/u/133774420?v=4",alt:"João Sousa"},argTypes:{src:{control:{type:"text"}}}},r={},a={args:{src:void 0}};var s,t,e;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};var o,c,p;a.parameters={...a.parameters,docs:{...(o=a.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    src: undefined
-  }
-}`,...(p=(c=a.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const g=["Primary","WithFallback"];export{r as Primary,a as WithFallback,g as __namedExportsOrder,l as default};
-//# sourceMappingURL=Avatar.stories-e1a0b0c4.js.map
